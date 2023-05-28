325a1bc02d4d723ac7ecf014328b2644af678206
Kyle Davis <kyledavis957@gmail.com>
2023-05-28 14:09:32 -0400

    Broken

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/about")
+def about()
+return "<p>About Me!</p>"
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
